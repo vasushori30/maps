6e2d83284d8932b9097ee7433178d62f72556b7f
瓦苏 <vasu.shori30@gmail.com>
2023-10-09 14:48:00 +0530

    other type of declaring map

--- main.go
+++ main.go
@@ -3,9 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	colors := map[string]string{
-		"red":   "#ff0000",
-		"green": "#4bf745",
-	}
+	colors := make(map[string]string)
+	colors["red"] = "#ff0000"
 	fmt.Println(colors)
 }
