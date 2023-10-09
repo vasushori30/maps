d375cdb83b993f4f610e117e01721dabd7ce3815
瓦苏 <vasu.shori30@gmail.com>
2023-10-09 14:50:10 +0530

    use of int as key in map

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	colors := make(map[string]string)
-	colors["red"] = "#ff0000"
-	fmt.Println(colors["red"])
+	colors := make(map[int]string)
+	colors[5] = "#ff0000"
+	fmt.Println(colors[5])
 }
